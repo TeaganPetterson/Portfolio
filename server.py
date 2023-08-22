e0153078889f8744513bade5c92a8f2f935ab17b
Teagan Petterson <teagan.petterson1@gmail.com>
2023-08-22 14:33:19 -0600

    added work page and route

--- server.py
+++ server.py
@@ -13,25 +13,30 @@ def homepage():
     
 	return render_template('index.html')
 
-@app.route('/contact_page')
+@app.route('/contact')
 def contact_page():
       
 	  return render_template('contact.html')
 
-@app.route('/about_page')
+@app.route('/about')
 def about_page():
       
 	  return render_template('about.html')
 
-@app.route('/experience_page')
+@app.route('/experience')
 def experience_page():
 
 	return render_template('experience.html')
 
-@app.route('/projects_page')
+@app.route('/projects')
 def projects_page():
 
 	return render_template('projects.html')
+
+@app.route('/work')
+def work_page():
+
+	return render_template('work.html')
 
 
 if __name__ == "__main__":
