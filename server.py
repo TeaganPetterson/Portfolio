2ee52ceabccd381e7c00f38e263b64afc81c10fe
Teagan Petterson <teagan.petterson1@gmail.com>
2024-01-23 19:13:12 -0700

    changed flask.service

--- server.py
+++ server.py
@@ -41,4 +41,5 @@ def work_page():
 
 if __name__ == "__main__":
     # connect_to_db(app)
-    app.run(host="0.0.0.0", debug=True)
+	from waitress import serve
+	serve(app, host="0.0.0.0", port=5000)
